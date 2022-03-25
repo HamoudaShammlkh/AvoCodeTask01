3e0e43d5eb362f510e5cc57b47481eebe1b59440
Hamouda Riad Shammlkh <hamoudashammlkh@gmail.com>
2022-03-25 15:11:17 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    <h1>hhhhhhhhhhhhhhhh</h1>
                 </div>
 
                 <div class="links">
